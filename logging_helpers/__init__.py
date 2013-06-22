a7a906278670f9186417ea628b5566c97e7f03ee
Christian Fobel <christian@fobel.net>
2013-06-22 08:35:59 -0400

    Add missing functions, add configure_logger,...
    Add missing functions (`callersname`, `whoami`).
    
    Add `configure_logger`, `get_log_level_parser_args`.

--- logging_helpers/__init__.py
+++ logging_helpers/__init__.py
@@ -1,4 +1,14 @@
+import sys
 import logging
+
+
+def whoami():
+    return sys._getframe(1).f_code.co_name
+
+# this uses argument 1, because the call to whoami is now frame 0.
+# and similarly:
+def callersname():
+    return sys._getframe(2).f_code.co_name
 
 
 def log_label(obj=None, function_name=True):
@@ -12,7 +22,8 @@ def log_label(obj=None, function_name=True):
     return '[%s]' % '.'.join(parts)
 
 
-def configure_logger(level):
+def configure_logger(level,
+                     format="%(asctime)s-%(name)s-%(levelname)s:%(message)s"):
     # if someone tried to log something before basicConfig is called, Python
     # creates a default handler that
     # goes to the console and will ignore further basicConfig calls. Remove the
@@ -21,5 +32,17 @@ def configure_logger(level):
     if root.handlers:
         for handler in root.handlers:
             root.removeHandler(handler)
-    logging.basicConfig(level=level,
-                        format="%(asctime)s-%(name)s-%(levelname)s:%(message)s")
+    logging.basicConfig(level=level, format=format)
+
+
+def get_log_level_parser_args(choices=('info', 'debug', 'warning', 'error',
+                                      'critical'), default_level='warning'):
+    '''
+    Can be used with configure_logger to set the log level using a command-line
+    argument.
+
+    For example:
+
+    >>>configure_logger(eval('logging.%s' % args.log_level.upper()))
+    '''
+    return ('--log_level', ), dict(choices=choices, default=default_level)
